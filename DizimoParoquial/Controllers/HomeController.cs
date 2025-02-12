89bd288cc366624ae28f08af72faad8d8ee367d3
Bruno Costa <brunogomesc@outlook.com.br>
2025-02-12 14:46:47 -0300

    feat: clean the folders structure of the project and crete the login view

--- DizimoParoquial/Controllers/HomeController.cs
+++ DizimoParoquial/Controllers/HomeController.cs
@@ -13,20 +13,9 @@ namespace DizimoParoquial.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string typeUser)
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ DizimoParoquial/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DizimoParoquial.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string user, string password)
+        {
+            return RedirectToAction("Index", "Home", new { typeUser = "admin"});
+        } 
+
+    }
+}
