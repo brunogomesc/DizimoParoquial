2122a0116b0129278685bd02825376eb4c56e21e
Bruno Costa <brunogomesc@outlook.com.br>
2025-02-17 22:35:05 -0300

    feat: create controllers and views

---
+++ DizimoParoquial/Controllers/PermissionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DizimoParoquial.Controllers
+{
+    public class PermissionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
