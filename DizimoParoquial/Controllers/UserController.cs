867b089b322fcbaee3a33c445dc6bd7fc852782e
Bruno Costa <brunogomesc@outlook.com.br>
2025-02-23 21:55:22 -0300

    feat: crete the register user view

--- DizimoParoquial/Controllers/UserController.cs
+++ DizimoParoquial/Controllers/UserController.cs
@@ -1,10 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DizimoParoquial.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DizimoParoquial.Controllers
 {
     public class UserController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SearchUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SaveUser(User usuario)
+        {
+
+            if (ModelState.IsValid)
+            {
+                // Aqui você pode salvar no banco de dados ou processar os dados
+                return RedirectToAction("Sucesso");
+            }
+            return View(usuario); // Retorna a mesma view caso haja erros de validação
+
+        }
+
+        [HttpPost]
+        public IActionResult EditUser()
         {
             return View();
         }
