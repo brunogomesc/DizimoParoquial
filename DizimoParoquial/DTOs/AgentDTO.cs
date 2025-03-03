304df7576ffb4770692a292f49d1fd64acda283a
Bruno Gomes <64209277+brunogomesc@users.noreply.github.com>
2025-03-03 19:52:52 -0300

    feat: create the process of Agents

---
+++ DizimoParoquial/DTOs/AgentDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace DizimoParoquial.DTOs
+{
+    public class AgentDTO
+    {
+
+        public int AgentId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int AgentCode { get; set; }
+
+        public bool Active { get; set; }
+
+    }
+}
