ae986db3aceffc03c11c492224483aac6cdb99e3
Bruno Gomes <64209277+brunogomesc@users.noreply.github.com>
2025-03-03 19:52:25 -0300

    feat: create the process of Agents

---
+++ DizimoParoquial/Models/Agent.cs
@@ -0,0 +1,19 @@
+﻿namespace DizimoParoquial.Models
+{
+    public class Agent
+    {
+
+        public int AgentId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public bool Active { get; set; }
+
+        public int AgentCode { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
