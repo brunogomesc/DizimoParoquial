65e11effc050bc391dd2a940e65b3064dedd5c53
Bruno Costa <brunogomesc@outlook.com.br>
2025-03-13 00:24:20 -0300

    feat: implement the new dashboard ValuePerPaymentType

---
+++ DizimoParoquial/Controllers/ReportController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DizimoParoquial.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult ReportValuePerPaymentType()
+        {
+            ViewBag.UserName = HttpContext.Session.GetString("Username");
+            return View();
+        }
+    }
+}
