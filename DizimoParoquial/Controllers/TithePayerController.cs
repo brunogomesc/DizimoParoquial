a9eadbc49323669699cf3e90b62e991f0fcd4159
Bruno Gomes <64209277+brunogomesc@users.noreply.github.com>
2025-03-16 23:29:18 -0300

    feat: auto search with the zipcode

--- DizimoParoquial/Controllers/TithePayerController.cs
+++ DizimoParoquial/Controllers/TithePayerController.cs
@@ -57,9 +57,12 @@ namespace DizimoParoquial.Controllers
             {
                 ViewBag.UserName = HttpContext.Session.GetString("Username");
 
-                if (string.IsNullOrWhiteSpace(tithePayer.Name) || string.IsNullOrWhiteSpace(tithePayer.Document))
+                if (string.IsNullOrWhiteSpace(tithePayer.Name) 
+                    || string.IsNullOrWhiteSpace(tithePayer.Document)
+                    || tithePayer.DateBirth != DateTime.MinValue
+                    || string.IsNullOrWhiteSpace(tithePayer.PhoneNumber))
                 {
-                    _notification.AddErrorToastMessage("Nome e Documento são obrigatórios!");
+                    _notification.AddErrorToastMessage("Nome, Documento, Data de Nascimento e Telefone são obrigatórios!");
                     return RedirectToAction(nameof(Index));
                 }
 
