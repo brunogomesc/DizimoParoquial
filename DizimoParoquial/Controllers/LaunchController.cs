c4b9832fc42bb627583ff7c3478a54f3ae40cd34
Bruno Costa <brunogomesc@outlook.com.br>
2025-05-07 23:10:25 -0300

    fix: button next page (pagination)

--- DizimoParoquial/Controllers/LaunchController.cs
+++ DizimoParoquial/Controllers/LaunchController.cs
@@ -193,13 +193,17 @@ namespace DizimoParoquial.Controllers
                     {
                         actualPage = Convert.ToInt32(buttonPage.Substring(0, (buttonPage.IndexOf("_")))) - 1;
                     }
+                    else if (page != null)
+                    {
+                        actualPage = Convert.ToInt32(page.Substring(0, (page.IndexOf("_"))));
+                    }
 
                     int pageSize = pageAmount != null ? Convert.ToInt32(pageAmount) : 10;
                     int count = 0;
                     string action = page is null ? "" : page.Substring(3, page.Length - 3);
                     int totalPages = tithePayers.Count % pageSize == 0 ? tithePayers.Count / pageSize : (tithePayers.Count / pageSize) + 1;
                     ViewBag.TotalPages = totalPages;
-                    ViewBag.ActualPage = 0;
+                    ViewBag.ActualPage = actualPage;
 
                     if (action.Contains("back") || action.Contains("next"))
                     {
@@ -452,13 +456,17 @@ namespace DizimoParoquial.Controllers
                     {
                         actualPage = Convert.ToInt32(buttonPage.Substring(0, (buttonPage.IndexOf("_")))) - 1;
                     }
+                    else if (page != null)
+                    {
+                        actualPage = Convert.ToInt32(page.Substring(0, (page.IndexOf("_"))));
+                    }
 
                     int pageSize = pageAmount != null ? Convert.ToInt32(pageAmount) : 10;
                     int count = 0;
                     string action = page is null ? "" : page.Substring(3, page.Length - 3);
                     int totalPages = tithePayers.Count % pageSize == 0 ? tithePayers.Count / pageSize : (tithePayers.Count / pageSize) + 1;
                     ViewBag.TotalPages = totalPages;
-                    ViewBag.ActualPage = 0;
+                    ViewBag.ActualPage = actualPage;
 
                     if (action.Contains("back") || action.Contains("next"))
                     {
@@ -675,13 +683,17 @@ namespace DizimoParoquial.Controllers
                     {
                         actualPage = Convert.ToInt32(buttonPage.Substring(0, (buttonPage.IndexOf("_")))) - 1;
                     }
+                    else if (page != null)
+                    {
+                        actualPage = Convert.ToInt32(page.Substring(0, (page.IndexOf("_"))));
+                    }
 
                     int pageSize = pageAmount != null ? Convert.ToInt32(pageAmount) : 10;
                     int count = 0;
                     string action = page is null ? "" : page.Substring(3, page.Length - 3);
                     int totalPages = tithes.Count % pageSize == 0 ? tithes.Count / pageSize : (tithes.Count / pageSize) + 1;
                     ViewBag.TotalPages = totalPages;
-                    ViewBag.ActualPage = 0;
+                    ViewBag.ActualPage = actualPage;
 
                     if (action.Contains("back") || action.Contains("next"))
                     {
